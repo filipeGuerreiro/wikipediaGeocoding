aefac61cc839d8524472b66a31e1603d13695da4
FilipeGuerreiro <filipe.m.guerreiro@gmail.com>
2015-10-14 13:45:07 +0100

    created base project

---
+++ proj/XmlParser.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 14, 2015
+
+@author: Filipe
+'''
+
+class XmlParser(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        

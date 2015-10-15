f9bfc27c9d5f5910f6aa851e173caeba6c442ee3
FilipeGuerreiro <filipe.m.guerreiro@gmail.com>
2015-10-15 21:19:46 +0100

    fixed previous commit

--- proj/main.py
+++ proj/main.py
@@ -8,19 +8,24 @@ import urllib2
 
 def getHyperlinkText(html_page):
     
-    soup = BeautifulSoup(html_page)
+    data = html_page.read()
+    
+    soup = BeautifulSoup(data)
     
     res = []
     for link in soup.findAll('a'):
-        res.append(link.get('title'))
+        
+        if link.get('title') != None:
+            #print link.get('title')
+            res.append(link.get('title'))
     
-    print '\n'.join(res)
+    #print ', '.join(res)
     return res
         
 
 if __name__ == '__main__':
-    pageName = "https://en.wikipedia.org/wiki/Riddley_Walker"
     
+    pageName = "https://en.wikipedia.org/wiki/Riddley_Walker"  
     html_page = urllib2.urlopen(pageName)
     
     hyperlinkList = getHyperlinkText(html_page)
